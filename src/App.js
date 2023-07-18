e430a32348fbd7d9f7c3d4fde86dbb0b444b162a
DEdmonds22 <ddedmonds22@gmail.com>
2023-07-18 13:43:54 -0400

    modified Nav component and App.js to render a nav bar

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
   return (
     <div>
       <Header />
+      <Nav />
       {article}
     </div>
   )
---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+
+function Nav() {
+    return (
+        <div className="nav-bar">
+            <ul>
+                <li>Women's</li>
+                <li>Men's</li>
+                <li>On the Street</li>
+                <li>The Catwalk</li>
+                <li>AdWatch</li>
+                <li>About</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Nav;
