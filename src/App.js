ac4eaecd465b4845827d74e02a8e0ca63dbb9884
DEdmonds22 <ddedmonds22@gmail.com>
2023-07-18 13:49:44 -0400

    modified Footer component and App.js to render a footer

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ function App() {
       <Header />
       <Nav />
       {article}
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+
+function Footer() {
+    return (
+        <footer>
+            <ul>
+                <li>Women's</li>
+                <li>Men's</li>
+                <li>On the Street</li>
+                <li>The Catwalk</li>
+                <li>AdWatch</li>
+                <li>About</li>
+            </ul>
+            <div>©2013 Valet Industries, Inc</div>
+        </footer>
+    )
+}
+
+export default Footer;
